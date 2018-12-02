7182856803dfd430cdcdb4c04cfe1837cbb85ffc
DESKTOP-I0OI5TR\Space Modal <craig.ellis@cditsolutions.com>
2018-12-02 14:47:52 -0600

    Added login and character creation page. Added fonts.

--- app/src/main/java/com/spacemodal/bossrush/LoginActivity.java
+++ app/src/main/java/com/spacemodal/bossrush/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.spacemodal.bossrush;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,45 +10,32 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setupLoginButton();
+    }
+
+    public void setupLoginButton(){
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                goToCharacterCreation();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void goToCharacterCreation(){
+        Intent characterCreationIntent = new Intent(getApplicationContext(), CharacterCreationActivity.class);
+        startActivity(characterCreationIntent);
+        finish();
     }
 }
